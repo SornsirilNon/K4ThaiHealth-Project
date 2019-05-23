6fca166c668a800187b504a6697a2ddd5a364890
Sornsiri <sornsiri.non@gmail.com>
2019-05-23 15:30:37 +0700

    Partial change by Jamie

--- ICD10_Browser-master/downloadable_file.php
+++ ICD10_Browser-master/downloadable_file.php
@@ -49,7 +49,7 @@
 
       <!-- Nav Item - Pages Collapse Menu -->
       <li class="nav-item">
-        <a class="nav-link collapsed" href="blank.html" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+        <a class="nav-link collapsed" href="blank.php" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
           <i class="fas fa-fw fa-folder"></i>
           <span>Browse</span>
         </a>
